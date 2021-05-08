45bb7c582b0783d0d87a51a6ee0d481d247d0286
Abderrahmane <abdrr97@gmail.com>
2021-05-08 05:37:24 +0000

    changin the laravel stub for make controller with  model generator file

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -3,12 +3,55 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Page;
 use Illuminate\Http\Request;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class PageController extends Controller
 {
 
-    public function index()
+    public function index(): View
     {
+        $pages = Page::all(); 
+
+        return view('pages.index', compact('pages'));
+    }
+
+    public function create(): View
+    {
+        return view('pages.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        Page::create($request->validated()); 
+
+        return redirect()->route('pages.index');
+    }
+
+    public function show(Page $page): View
+    {
+        return view('pages.show',compact('pages'));
+    }
+
+    public function edit(Page $page): View
+    {
+        return view('pages.edit',compact('pages'));
+    }
+
+    public function update(Request $request, Page $page): RedirectResponse
+    {
+       $page->update($request->validated()); 
+
+        return redirect()->route('pages.index',compact('pages'));
+    }
+
+    public function destroy(Page $page): RedirectResponse
+    {
+        $page->delete();
+
+        return redirect()->route('pages.index');
     }
 }
